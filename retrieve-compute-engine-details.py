ae7a976cc70fa7d82ba4afad4736ebdc66145d51
virattGoogle <95920617+virattGoogle@users.noreply.github.com>
2023-05-10 11:20:05 -0400

    Update retrieve-compute-engine-details.py

--- retrieve-compute-engine-details.py
+++ retrieve-compute-engine-details.py
@@ -12,15 +12,15 @@ import os
 credentials = GoogleCredentials.get_application_default()
 service = discovery.build('compute', 'v1', credentials=credentials)
  
-#if (len(sys.argv)) >=3:
-    #project=sys.argv[1]
-  #  instance = sys.argv[2]
- #   zone = sys.argv[3]
-  #  status = sys.argv[4]
+if (len(sys.argv)) >=3:
+    project=sys.argv[1]
+    instance = sys.argv[2]
+    zone = sys.argv[3]
+   status = sys.argv[4]
  #   patchid = sys.argv[5]
- #   pzone=''
- #   pstate=''
- #   preason=''
+   pzone=''
+   pstate=''
+   preason=''
  
     # # Get instance details
     request = service.instances().get(project=project, zone=zone, instance=instance)
